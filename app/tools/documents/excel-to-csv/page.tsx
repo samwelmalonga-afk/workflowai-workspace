7b5df9f17c6e3b74e8866221211227c64ebdd7d6
samwelmalonga-afk <samwelmalonga@gmail.com>
2026-04-16 19:04:50 -0400

    Update page.tsx

--- app/tools/documents/excel-to-csv/page.tsx
+++ app/tools/documents/excel-to-csv/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import ToolLayout from '../../../components/ToolLayout'
+import ToolLayout from '@/components/ToolLayout'
 
 export default function ExcelToCSV() {
   const [file, setFile] = useState<File | null>(null)
